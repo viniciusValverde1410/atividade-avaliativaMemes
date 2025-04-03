d88a3c3f393372dfdb795b2c497a63fd00d04e9f
Vinicius Valverde <vinicius.valverde@aluno.senai.br>
2025-04-03 14:40:23 -0300

    feat: create Hero Section  component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 import styles from "./page.module.css";
 import Header from "./components/header";
+import HeroSection from "./components/heroSection";
 
 export default function Home() {
   // Array de dados dos memes para serem passados como props
@@ -72,19 +73,6 @@ export default function Home() {
       category: "Relacionamentos",
     },
   ];
-
-  // Dados para o meme do dia (hero section)
-  const memeOfTheDay = {
-    id: 7,
-    title: "Meme do Dia: Quando seu código funciona de primeira",
-    description:
-      "Este momento raro que todos os desenvolvedores sonham em experimentar!",
-    image: "https://i.imgur.com/vldGJnz.jpg",
-    likes: 2452,
-    comments: 387,
-    author: "SuperCoder",
-    authorAvatar: "https://i.pravatar.cc/150?img=20",
-  };
 
   // Array de criadores em destaque
   const topCreators = [
@@ -175,36 +163,14 @@ export default function Home() {
 
       <div className={styles.mainContent}>
         <div className={styles.contentArea}>
-          {/* COMPONENTE: HeroSection */}
-          <section className={styles.heroSection}>
-            <div className={styles.heroContent}>
-              <span className={styles.heroTag}>Meme do Dia</span>
-              <h2 className={styles.heroTitle}>{memeOfTheDay.title}</h2>
-              <p className={styles.heroDescription}>
-                {memeOfTheDay.description}
-              </p>
-              <div className={styles.heroAuthor}>
-                <img
-                  src={memeOfTheDay.authorAvatar}
-                  alt={memeOfTheDay.author}
-                />
-                <span>Por {memeOfTheDay.author}</span>
-              </div>
-              <div className={styles.heroStats}>
-                <span>❤️ {memeOfTheDay.likes}</span>
-                <span>💬 {memeOfTheDay.comments}</span>
-              </div>
-              <button className={styles.heroButton}>Ver meme completo</button>
-            </div>
-            <div className={styles.heroImageContainer}>
-              <img
-                src={memeOfTheDay.image}
-                alt={memeOfTheDay.title}
-                className={styles.heroImage}
-              />
-            </div>
-          </section>
-          {/* FIM COMPONENTE: HeroSection */}
+
+        <HeroSection author={"SuperCoder"}
+            authorAvatar={"https://i.pravatar.cc/150?img=20"}
+            comments={"387"}
+            description={"Este momento raro que todos os desenvolvedores sonham em experimentar!"}
+            image={"https://i.imgur.com/vldGJnz.jpg"}
+            likes={2452}
+            title={"Meme do Dia: Quando seu código funciona de primeira"} />
 
           {/* COMPONENTE: CategoriesSection */}
           <section className={styles.categoriesSection}>
