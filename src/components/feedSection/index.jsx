13f24d0cb27027821d5db8160eae69123b9655b1
vinicius valverde <vinicius.valverde@aluno.senai.br>
2025-04-14 01:01:49 -0300

    feat: create meme card component

--- src/components/feedSection/index.jsx
+++ src/components/feedSection/index.jsx
@@ -1,4 +1,5 @@
 import styles from "./feedSection.module.css";
+import MemeCard from "../memeCard";
 
 const FeedSection = ( {memes} ) => {
     return (
@@ -18,46 +19,7 @@ const FeedSection = ( {memes} ) => {
           {/* Aqui mapeamos os memes do array para criar múltiplos cards */}
           {memes.map((meme) => (
             // COMPONENTE: MemeCard
-            // <MemeCard />
-            <div key={meme.id} className={styles.memeCard}>
-              <div className={styles.memeCardHeader}>
-                <div className={styles.memeAuthor}>
-                  <img src={meme.authorAvatar} alt={meme.author} />
-                  <span>{meme.author}</span>
-                </div>
-                <span className={styles.memeCategory}>{meme.category}</span>
-              </div>
-              <img
-                src={meme.image}
-                alt={meme.title}
-                className={styles.memeImage}
-              />
-              <div className={styles.memeContent}>
-                <h3 className={styles.memeTitle}>{meme.title}</h3>
-                <p className={styles.memeDescription}>{meme.description}</p>
-
-                {/* COMPONENTE: InteractionBar */}
-                <div className={styles.interactionBar}>
-                  <div className={styles.interactionButton}>
-                    <span>👍</span>
-                    <span>{meme.likes}</span>
-                  </div>
-                  <div className={styles.interactionButton}>
-                    <span>💬</span>
-                    <span>{meme.comments}</span>
-                  </div>
-                  <div className={styles.interactionButton}>
-                    <span>🔄</span>
-                    <span>Share</span>
-                  </div>
-                  <div className={styles.interactionButton}>
-                    <span>🔖</span>
-                    <span>Save</span>
-                  </div>
-                </div>
-                {/* FIM COMPONENTE: InteractionBar */}
-              </div>
-            </div>
+            <MemeCard meme={meme} key={meme.id}/>
             // FIM COMPONENTE: MemeCard
           ))}
         </div>
