679fda867c55db3f3cc7976f44226d09a842d795
vinicius valverde <vinicius.valverde@aluno.senai.br>
2025-04-14 01:18:40 -0300

    feat: create featuredMemeCard component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,7 @@ import Sidebar from "../components/sidebar";
 import HeroSection from "../components/heroSection";
 import CategoriesSection from "../components/categoriesSection";
 import FeedSection from "../components/feedSection";
+import FeaturedSection from "../components/featuredSection";
 
 export default function Home() {
 
@@ -200,38 +201,7 @@ export default function Home() {
           {/* FIM COMPONENTE: Feed */}
 
           {/* COMPONENTE: FeaturedMemesSection */}
-          <section className={styles.featuredSection}>
-            <h2 className={styles.sectionTitle}>Memes em Destaque</h2>
-            <div className={styles.featuredGrid}>
-              {featuredMemes.map((meme) => (
-                // COMPONENTE: FeaturedMemeCard
-                <div key={meme.id} className={styles.featuredCard}>
-                  <div className={styles.featuredImageContainer}>
-                    <img
-                      src={meme.image}
-                      alt={meme.title}
-                      className={styles.featuredImage}
-                    />
-                    {meme.trending && (
-                      <span className={styles.trendingBadge}>🔥 Trending</span>
-                    )}
-                  </div>
-                  <div className={styles.featuredContent}>
-                    <h3 className={styles.featuredTitle}>{meme.title}</h3>
-                    <div className={styles.featuredInfo}>
-                      <span className={styles.featuredAuthor}>
-                        Por {meme.author}
-                      </span>
-                      <span className={styles.featuredCategory}>
-                        {meme.category}
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                // FIM COMPONENTE: FeaturedMemeCard
-              ))}
-            </div>
-          </section>
+          <FeaturedSection featuredMemes = {featuredMemes} />
           {/* FIM COMPONENTE: FeaturedMemesSection */}
 
           {/* COMPONENTE: CreatorsSection */}
