927967f6e73e2856c5bc480cce5d5cf678b36f64
vinicius valverde <vinicius.valverde@aluno.senai.br>
2025-04-14 01:36:32 -0300

    feat: create newsletterSection component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,6 +6,7 @@ import CategoriesSection from "../components/categoriesSection";
 import FeedSection from "../components/feedSection";
 import FeaturedSection from "../components/featuredSection";
 import CreatorsSection from "../components/creatorsSection";
+import NewsletterSection from "../components/newsletterSection";
 
 export default function Home() {
 
@@ -208,32 +209,7 @@ export default function Home() {
           {/* FIM COMPONENTE: CreatorsSection */}
 
           {/* COMPONENTE: NewsletterSection */}
-          <section className={styles.newsletterSection}>
-            <div className={styles.newsletterContent}>
-              <h2 className={styles.newsletterTitle}>Fique por dentro!</h2>
-              <p className={styles.newsletterDescription}>
-                Assine nossa newsletter e receba os melhores memes toda semana!
-              </p>
-              <div className={styles.newsletterForm}>
-                <input
-                  type="email"
-                  placeholder="Seu melhor e-mail"
-                  className={styles.newsletterInput}
-                />
-                <button className={styles.newsletterButton}>Assinar</button>
-              </div>
-              <p className={styles.newsletterDisclaimer}>
-                Nós respeitamos sua privacidade. Cancele quando quiser.
-              </p>
-            </div>
-            <div className={styles.newsletterImageContainer}>
-              <img
-                src="https://i.imgur.com/OELrGl5.jpg"
-                alt="Newsletter"
-                className={styles.newsletterImage}
-              />
-            </div>
-          </section>
+          <NewsletterSection />
           {/* FIM COMPONENTE: NewsletterSection */}
         </div>
 
